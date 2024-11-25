45e6d1a8766a7575bfabd743a94a568a7aae71fa
Sophie <szoph101@hotmail.co.uk>
2024-11-25 14:38:06 +0000

    add files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+        SHOPIFY_STOREFRONT_API_TOKEN: process.env.SHOPIFY_STOREFRONT_API_TOKEN
+    }, 
+    images: {
+        domains: ['cdn.shopify.com'],
+    }
+}
